bea230ab2f30d97822982ee48c12238602e20fd6
Nafis Sadat <nasadat@microsoft.com>
2019-10-24 10:33:53 -0700

    Use inner matx and remove commented code with Discreture from cpp

--- dgdecomp/R/Func_Inner_Matx.R
+++ dgdecomp/R/Func_Inner_Matx.R
@@ -2,10 +2,10 @@
 #'
 #' @export
 #'
-Func_Inner_Matx <- function(P, r, vec_x, vec_y, ...) {
+Func_Inner_Matx <- function(P, r, vec_x, vec_y) {
   return(
     .Call(
-      "ArmaNum", P, r, vec_x, vec_y
-    ) / Func_Dem(P, r)
+      "ArmaInner", P, r, vec_x, vec_y
+    )
   )
 }
--- dgdecomp/R/Func_Inner_Sum_Matx.R
+++ dgdecomp/R/Func_Inner_Sum_Matx.R
@@ -8,23 +8,8 @@ Func_Inner_Sum_Matx <- function(P, vec_x, vec_y) {
   sum_count <- rep(0, nrow(vec_x))
 
   for (r in 1:P_upper) {
-    if (r > 1) {
-      ## Create positional vectors
-      size1 <- P - r ## r < P and so will never be >= P-1
-      size2 <- r - 1 ## Will never be <= 1 because r > 1
-      posit_seqs <- Func_Create_Combn(ncol(vec_x), size1, size2)
-    } else {
-      posit_seqs <- list(
-        "vec_x_pos" = as.matrix(0),
-        "vec_y_pos" = as.matrix(0)
-      )
-    }
-
-
     sum_count <- sum_count + Func_Inner_Matx(
-      P, r, vec_x, vec_y,
-      posit_seqs$vec_x_pos,
-      posit_seqs$vec_y_pos
+      P, r, vec_x, vec_y
     )
   }
   return(sum_count)
